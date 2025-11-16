26c593576ed56df1b80e2434347c3836d8d86aac
clonefuture <clonefuture@gmail.com>
2025-11-16 13:25:24 +0200

    Добавить тесты валидации

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmValidationTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class FilmControllerTest {
+class FilmValidationTest {
 
     private FilmController filmController;
     private LocalDate minReleaseDate;
