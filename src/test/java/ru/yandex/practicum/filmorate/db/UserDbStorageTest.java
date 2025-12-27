c70a7c68be17d87fdea920e39002a7011aa19b83
unknown <kukuruzco@yandex.ru>
2025-12-27 11:11:40 +0200

    Внести правки согласно комментариям 2

--- src/test/java/ru/yandex/practicum/filmorate/db/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/db/UserDbStorageTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.db.UserDbStorage;
 
